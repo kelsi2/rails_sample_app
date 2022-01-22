40e5e2af7b39bc74d8b16172d9ba602f26292f7e
Kelsi Proulx <kelsiproulx1919@gmail.com>
2022-01-22 15:04:30 -0800

    Implementing friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,7 @@ module SessionsHelper
     end
   end
 
-  def current_user(user)
+  def current_user?(user)
     user && user == current_user
   end
 
@@ -39,5 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
