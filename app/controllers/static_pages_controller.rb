37b78fe1a678988f42dacfb1b2ef2ea3300408c5
Kelsi Proulx <kelsiproulx1919@gmail.com>
2022-02-07 14:46:52 -0800

    Refactor to make tests pass

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   # This empty function will automatically try to render a view with the corresponding name
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
