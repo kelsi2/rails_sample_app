894c6c05b4be78640e7bf74e44eaf6c4378366b3
Kelsi Proulx <kelsiproulx1919@gmail.com>
2022-02-07 15:04:15 -0800

    Render micropost feed on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
   # This empty function will automatically try to render a view with the corresponding name
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
